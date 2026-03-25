d13e74b367db6fba12e254f16351e179d98001e5
D' New x86 <mendieta.c.gamer@gmail.com>
2026-03-24 21:45:26 -0500

    .

--- ppv1_portafolio/conf_rutas.py
+++
@@ -1,2 +0,0 @@
-LOGO_URL = "https://raw.githubusercontent.com/dnewx86/dnewx86/main/ppv1_portafolio/img/logo.png"
-NO_IMAGEN_URL = "https://raw.githubusercontent.com/dnewx86/dnewx86/main/ppv1_portafolio/img/no_imagen.png"
--- ppv1_portafolio/utils.py
+++
@@ -1,6 +0,0 @@
-import re
-
-
-def email_valido(email: str) -> bool:
-    patron = r'^[\w\.-]+@[\w\.-]+\.\w+$'
-    return bool(re.match(patron, email))
